107d7c034bbd3a70bd4866d60f36346ca4aec795
nara <parknara0213@gmail.com>
2024-08-27 19:31:21 +0900

    240827 마이페이지 신고 카드 변경

--- src/pages/Mypage/MypageReport/components/MypageReportCard.tsx
+++ src/pages/Mypage/MypageReport/components/MypageReportCard.tsx
@@ -15,49 +15,73 @@ const MypageReportCard = ({ item, items, setItems }: mypageReportCardProps) => {
   return (
     <li className="mypage-report-card">
       <div className="mypage-report-card-container">
-        <div className="mypage-report-card-upper">
-          <span className="mypage-report-card-info">
-            <div className="mypage-report-card-info-reportType">
-              <button
-                className={`mypage-report-card-info-reportType-button ${
-                  item.reportReason === "음란"
-                    ? "lewd"
-                    : item.reportReason === "폭력"
-                    ? "violence"
-                    : item.reportReason === "욕설"
-                    ? "abuse"
-                    : "etc"
+        <div className="mypage-report-card-item">
+          <span className="mypage-report-card-item-name">신고한 사람</span>
+          <span className="mypage-report-card-item-detail">
+            {item.blockUserNickname}
+          </span>
+        </div>
+        <div className="mypage-report-card-item">
+          <span className="mypage-report-card-item-name">신고 당한 사람</span>
+          <span className="mypage-report-card-item-detail">
+            {item.blockedUserNickname}
+          </span>
+        </div>
+        <div className="mypage-report-card-item">
+          <span className="mypage-report-card-item-name">차단 날짜</span>
+          <span className="mypage-report-card-item-detail">
+            {convertYYYYMMDDToDate1(item.reportDate)}
+          </span>
+        </div>
+        <div className="mypage-report-card-item">
+          <span className="mypage-report-card-item-name">신고글</span>
+          <span className="mypage-report-card-item-detail">
+            {item.postTitle}
+          </span>
+        </div>
+        <div className="mypage-report-card-item">
+          <span className="mypage-report-card-item-name">신고 내용</span>
+          <span className="mypage-report-card-item-detail">
+            <button
+              className={`mypage-report-card-info-reportType-button ${
+                item.reportType === "R1"
+                  ? "lewd"
+                  : item.reportType === "R2"
+                  ? "violence"
+                  : item.reportType === "R3"
+                  ? "abuse"
+                  : "etc"
+              }`}
+            >
+              {item.reportType === "R1"
+                ? "음란"
+                : item.reportType === "R2"
+                ? "폭력"
+                : item.reportType === "R3"
+                ? "욕설"
+                : "기타"}
+            </button>
+            {item.reportDetail && (
+              <span
+                className={`mypage-report-card-info-reportType-dropdown${
+                  detail ? " active" : ""
                 }`}
+                onClick={() => setDetail(!detail)}
               >
-                {item.reportReason}
-              </button>
-              {item.reportDetail && (
-                <span
-                  className={`mypage-report-card-info-reportType-dropdown${
-                    detail ? " active" : ""
-                  }`}
-                  onClick={() => setDetail(!detail)}
-                >
-                  <IoIosArrowDropup />
-                </span>
-              )}
-            </div>
-            <div className="mypage-report-card-info-reportId">
-              <span className="mypage-report-card-info-reportId-name">
-                신고글 :{" "}
+                <IoIosArrowDropup />
               </span>
-              <span className="mypage-report-card-info-reportId-detail">
-                {item.postTitle}
-              </span>
-            </div>
-            <div className="mypage-report-card-info-date">
-              <span>신고 날짜 : </span>
-              <span>{convertYYYYMMDDToDate1(item.reportDate)}</span>
-            </div>
+            )}
           </span>
-          <span className="mypage-report-card-btn">
-            <button>처리중</button>
-          </span>
+        </div>
+
+        <div className="mypage-report-card-btn">
+          <button>
+            {item.reportFalse === 1
+              ? "처리 완료"
+              : item.reportFalse === 2
+              ? "허위 신고"
+              : "처리중"}
+          </button>
         </div>
         <div className={`mypage-report-card-lower${detail ? " active" : ""}`}>
           <div className="mypage-report-card-lower-container">
